bf2c9c6a4f903dd61c860ee54fddc7f325f17cac
kim sungkuk <sungkuk5420@gmail.com>
2023-12-20 11:14:27 +0900

    마이페이지 UI : VueRouter 중첩 레이아웃 구성

--- src/pages/mypage/bookmark.vue
+++ src/pages/mypage/bookmark.vue
@@ -1,13 +1,25 @@
 <template>
-  <div>
-    <div class="text-h3">mypage/bookmark.vue</div>
-  </div>
+  <PostList :items="posts"> </PostList>
 </template>
 
 <script setup>
+import PostList from "src/components/apps/posts/PostList.vue";
 
+const posts = Array.from(Array(3), (_, index) => {
+  return {
+    id: "A" + index,
+    title: "Vue3 Firebase 강의" + index,
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos molestiae quibusdam nobis facere, at laborum corrupti rerum blanditiis itaque libero repellat quia quidem cumque culpa repudiandae vel, quaerat quis dolorem.",
+    readCount: 1,
+    commentCount: 2,
+    likeCount: 3,
+    bookmarkCount: 4,
+    tags: ["html", "css", "javascript"],
+    uid: "uid",
+    category: "카테고리" + index,
+  };
+});
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
